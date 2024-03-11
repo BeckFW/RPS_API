f3604c54d22e1c2f12763549d42eccfc3501e54e
BeckFW <beck@elbo.me>
2024-03-11 19:18:45 +0000

    Allow localhost through cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Rock Paper Scissors API // 
 
 const express = require("express"); 
+const cors = require("cors"); 
 const app = express(); 
 const PORT = process.env.PORT || 3100; 
 
 // {} import due to multiple exports from move.js
 const  { router: move } = require('./routes/move');
 const { router: gestures } = require('./routes/gestures');
+
+// Allow local origin through cors
+app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use('/gestures', gestures);
 app.use('/moves', move); 
