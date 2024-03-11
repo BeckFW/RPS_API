3699f4af044d2299a6f995e3b33e9bd2566f5c26
BeckFW <beck@elbo.me>
2024-03-11 13:01:07 +0000

    Format and Process image sent to python via stdin

--- scripts/recognize_gesture.py
+++ scripts/recognize_gesture.py
@@ -3,26 +3,42 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from PIL import Image
 from io import BytesIO
+import numpy as np
 import sys
 
+# Rock Paper Scissors model for mediapipe
 model_path = './models/gesture_recognizer_rps.task'
 
+# MediaPipe Variables
 base_options = python.BaseOptions(model_asset_path=model_path)
 options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options) 
 
-# Get the image
-try:
-    image = BytesIO(sys.argv[1].encode())
-    image_to_process = mp.Image.create_from_file(image)
+## Access and Format Image ## 
+try: 
+    # Get the buffer from stdin
+    image_buffer = sys.stdin.buffer.read()
+
+    # convert to an image
+    image = Image.open(BytesIO(image_buffer))
+
+    # format into mediapipe image class
+    image_to_process = mp.Image(
+        image_format=mp.ImageFormat.SRGBA, data=np.asarray(image))   
+    
 except Exception as e: 
     print("Image Exception: ", e)
-# Process the image
+
+## Process the Image ##
 try:
+    # Run gesture recognizer
     result = recognizer.recognize(image_to_process)
+
+    # Send result back to Node.js
     print(result.gestures[0][0].category_name)
+
 except Exception as e:
     print("Exception: ", e)
-# Return result
 
+# Ensure any results are sent back by flushing stdout
 sys.stdout.flush()
