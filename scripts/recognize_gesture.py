d85d9ebfb5c7f1f6127178ef7c3fafb1cac0e15e
BeckFW <beck@elbo.me>
2024-04-09 14:43:40 +0100

    Lower detection confidence - Add extra logs

--- scripts/recognize_gesture.py
+++ scripts/recognize_gesture.py
@@ -6,12 +6,15 @@ from io import BytesIO
 import numpy as np
 import sys
 
+
 # Rock Paper Scissors model for mediapipe
 model_path = './models/gesture_recognizer_rps.task'
 
 # MediaPipe Variables
 base_options = python.BaseOptions(model_asset_path=model_path)
-options = vision.GestureRecognizerOptions(base_options=base_options)
+options = vision.GestureRecognizerOptions(
+    base_options=base_options, 
+    min_hand_presence_confidence=0.3)
 recognizer = vision.GestureRecognizer.create_from_options(options) 
 
 ## Access and Format Image ## 
@@ -21,10 +24,12 @@ try:
     #print(image_buffer)
     # convert to an image
     image = Image.open(BytesIO(image_buffer))
+    print('Python: Image Received')
 
     # format into mediapipe image class
     image_to_process = mp.Image(
-        image_format=mp.ImageFormat.SRGBA, data=np.asarray(image))   
+        image_format=mp.ImageFormat.SRGBA, data=np.asarray(image))
+    print('Python: Image Formatted')   
     
 except Exception as e: 
     print("Exception:", e)
@@ -33,9 +38,10 @@ except Exception as e:
 try:
     # Run gesture recognizer
     result = recognizer.recognize(image_to_process)
+    print(result)
 
     # Send result back to Node.js
-    print(str(result.gestures[0][0].category_name))
+    print('move: ', str(result.gestures[0][0].category_name))
 
 except Exception as e:
     print("Exception: ", e)
