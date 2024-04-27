80d984ad88e0920f8d5cb0b5791fe445c7bc3a70
BeckFW <beck@elbo.me>
2024-04-27 14:24:33 +0100

    Update environment variables

--- index.js
+++ index.js
@@ -3,18 +3,22 @@
 const express = require("express"); 
 const cors = require("cors"); 
 const app = express(); 
-const PORT = process.env.PORT || 3100; 
 
-// {} import due to multiple exports from move.js
+// Environment Variables //
+const PORT = process.env.PORT || 3200; 
+const ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
+const API_KEY = process.env.API_KEY;
+
+// {} import due to multiple exports from routes
 const  { router: move } = require('./routes/move');
 const { router: gestures } = require('./routes/gestures');
 
 // Allow local origin through cors
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: ORIGIN}));
 
 // Check all routes for an API key held as an environment variable
 app.use((req, res, next) => {
-    if (req.headers['api-key'] !== process.env.API_KEY) {
+    if (req.headers['api-key'] !== API_KEY) {
         res.status(401).send("Not authorised");
     } else {
         next();
